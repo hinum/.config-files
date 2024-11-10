7a3b26a7fe4642baa547f8101572e5425ef84dfc
hinum <visutshu@gmail.com>
2024-11-10 19:11:08 +0700

    added gopls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -63,6 +63,7 @@ return {
       require('cmp_nvim_lsp').default_capabilities())
 
     vim.g.markdown_fenced_languages = { "ts=typescript" }
+    lspconf.gopls.setup{}
     lspconf.denols.setup{
       root_dir = lspconf.util.root_pattern("deno.json", "deno.jsonc"),
     }
